428d827a64765a3972f1c2129b249869971abf2b
Dennis Lysenko <dennis.s.lysenko@gmail.com>
2014-12-22 12:03:54 -0500

    more docs

--- remove_duplicates.rb
+++ remove_duplicates.rb
@@ -1,9 +1,22 @@
-# Recursively crawls through a directory and deletes singular mac-style duplicate files.
+# Recursively crawls through a directory and deletes singular duplicate files.
 # Useful for when you accidentally hit "Consolidate files" on your entire iTunes library.
-# THIS IS A POTENTIALLY DESTRUCTIVE SCRIPT -- keep $MAX_FILES_TO_DELETE low at first to 
-# get a feel for the exact files it's deleting -- MAKE SURE THEY ARE THE RIGHT ONES.
-$TARGET_DIR = "/Users/dennis/Dropbox/Music"
-$MAX_FILES_TO_DELETE = 10
+# THIS IS A POTENTIALLY DESTRUCTIVE SCRIPT -- keep $MAX_FILES_TO_DELETE low (10) at first
+# to get a feel for the exact files it's deleting -- MAKE SURE THEY ARE THE RIGHT ONES.
+
+
+                    ####################################
+                    # NO WARRANTY - EXPRESS OR IMPLIED #
+                    #       RUN AT YOUR OWN RISK!      #
+                    ####################################
+
+
+# Configuration
+$TARGET_DIR = '/Users/dennis/Dropbox/Music' # the directory to process
+$MAX_FILES_TO_DELETE = 10                   # the maximum amount of files it will delete (best to cap at 10 initially, then 1000)
+$DUPLICATE_INDICATOR = ' 1'                 # the suffix indicating a duplicate (if it finds "Ahiga.mp3", it will try to delete "Ahiga$DUPLICATE_INDICATOR.mp3")
+
+
+# End configuration
 $dups = []
 
 def process_dir(dir_raw)
@@ -20,7 +33,7 @@ def process_dir(dir_raw)
             dot_index = cur_path.rindex('.')
 
             unless dot_index.nil?
-                dup_file_path = cur_path[0...dot_index] + ' 1' + cur_path[dot_index...cur_path.length]
+                dup_file_path = cur_path[0...dot_index] + $DUPLICATE_INDICATOR + cur_path[dot_index...cur_path.length]
                 $dups << dup_file_path unless dir.find_index(File.basename(dup_file_path)).nil?
             end
         end
